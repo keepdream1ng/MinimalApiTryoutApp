68989febc10c0ab41624c33e8076a40af93d0f2c
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-12 19:22:07 +0530

    Add project files.

---
+++ MinimalApiTryout/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.MapGet("/api/{parameter}", (string parameter) =>
+{
+    return $"Parameter from the route: {parameter}";
+});
+
+app.Run();
