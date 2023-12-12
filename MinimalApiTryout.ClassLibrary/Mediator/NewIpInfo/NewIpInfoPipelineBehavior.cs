3675061e0e14390974ccec75338fdc6d44ae5e8a
Anatolii Ivanov <gotnickname@gmail.com>
2023-12-12 21:22:21 +0530

    added pipeline behavior

---
+++ MinimalApiTryout.ClassLibrary/Mediator/NewIpInfo/NewIpInfoPipelineBehavior.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MinimalApiTryout.ClassLibrary.Mediator;
+public class NewIpInfoPipelineBehavior : IPipelineBehavior<NewIpInfoRequest, string>
+{
+	public Task<HttpResponseMessage> Handle(NewIpInfoRequest request, RequestHandlerDelegate<string> next, CancellationToken cancellationToken)
+	{
+		Console.WriteLine(request.Ip);
+		return next();
+	}
+}
